ec2828aea3951592e683ebdae08eaebd7095849d
Linh Tran <linh.mtran168@live.com>
2014-01-16 13:49:12 +0700

    update deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -60,8 +60,8 @@ namespace :deploy do
   desc 'Setup nginx and unicorn configuration'
   task :setup_config do
     on roles(:app) do
-      sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{fetch(:application)}"
-      sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{fetch(:application)}"
+      execute "sudo /bin/ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{fetch(:application)}"
+      execute "sudo /bin/ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{fetch(:application)}"
     end
   end
 
